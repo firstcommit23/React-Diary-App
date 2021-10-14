42ae0c24d3c543612f3f252a036e202385f576e7
jimin <jm131313.kim@gmail.com>
2021-10-14 13:29:20 +0900

    Add: 일기 작성화면 기능구현(컴포넌트 분리, 리펙토링 전)

---
+++ src/components/write/Editor.tsx
@@ -0,0 +1,5 @@
+import React, { useRef, useEffect } from 'react';
+// import Quill from 'quill';
+import '/quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+import palette from '../../styles/palette';
